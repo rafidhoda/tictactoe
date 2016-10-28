fba1bb213946ad77f302ae2a64df25cf5a0e66b0
Rafid Hoda <rafidhoda@gmail.com>
2016-10-28 04:19:42 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      PLAYER_ONE_SYMBOL: "X",
+      PLAYER_TWO_SYMBOL: "O",
+      currentTurn: "X",
+      board: [
+        "", "", "", "", "", "", "", "", ""
+      ]
+    }
+  }
+
+  handleClick(index) {
+    if(this.state.board[index] === "") {
+      this.state.board[index] = this.state.currentTurn
+      this.setState({
+        board: this.state.board,
+        currentTurn: this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL ? this.state.PLAYER_TWO_SYMBOL : this.state.PLAYER_ONE_SYMBOL
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="board">
+        {this.state.board.map((cell, index) => {
+          return <div onClick={() => this.handleClick(index)} className="square">{cell}</div>
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
